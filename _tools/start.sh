ab6503e4c19428771cffa3d7ad653b20c0dc8c5c
Issei Suzuki <4515431+isseis@users.noreply.github.com>
2020-09-25 13:06:48 +0200

    Use bundler installed by rbenv, not system default one. [ci skip]

--- _tools/start.sh
+++ _tools/start.sh
@@ -1,9 +1,4 @@
 #! /bin/sh
 
-bundle_cmd="bundle"
-if [ -x /usr/bin/bundle2.7 ]; then
-	bundle_cmd="/usr/bin/bundle2.7"
-fi
-
 # Start Jekyll server
-exec ${bundle_cmd} exec jekyll s --drafts --livereload
+exec bundle exec jekyll s --drafts --livereload
